1e5fa8430ab282c92782d5553fb0faf0f1920eb2
Miguel-CaAr <castroarredondo.hector@gmail.com>
2023-10-03 14:12:31 -0700

    Finalizado

---
+++ script.js
@@ -0,0 +1,35 @@
+const n = [1, 2, 33, 12, 5];
+const arregloFilter = [];
+
+n.filter((numero) => {
+  if (numero >= 5){
+    arregloFilter.push(numero);
+  }
+});
+
+arregloFilter.forEach((numero) => console.log(numero))
+
+//EJERCICIO 2
+
+const frutas = ['naranja', 'platano', 'manzana', 'fresa', 'kiwi'];
+const arregloSlice = frutas.slice(2, 4);
+console.log("Arreglo: " + JSON.stringify(arregloSlice))
+
+//EJERCICO 3
+
+const arreglo3 = [10, 5, 10];
+arreglo3.map((elemento, index)=>{
+    console.log((elemento+10) + `:${index}`)
+})
+
+//EJERCICIO 4
+
+const letras1 = ['H', 'I', 'J'];
+const letras2 = ['K', 'L', 'M'];
+letrasOrdenadas = letras1.concat(letras2);
+console.log(letrasOrdenadas);
+
+//EJERCICIO 5
+
+const paises = ["Mexico", "USA", "Francia"];
+console.log(paises.includes("Francia"));
